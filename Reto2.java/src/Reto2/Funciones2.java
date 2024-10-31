6626b2df0997f34f34e76b870c0de10bc575bf07
DAM1 <DAM1@aula11-pc18>
2024-10-31 09:14:09 +0100

    He creado la funcion celsius a fahrenheit

--- Reto2.java/src/Reto2/Funciones2.java
+++ Reto2.java/src/Reto2/Funciones2.java
@@ -1,5 +1,27 @@
 package Reto2;
 
-public class Funciones2 {
+import java.util.Scanner;
 
+import util.Funciones;
+
+public class Funciones2 {
+	
+	public static Double celsiusAfahrenhit(Double celsius) {
+		return celsius*1.8+32;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
