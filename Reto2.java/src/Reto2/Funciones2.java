ea6f40ff71d25091759ca79e8a0b60a2b13b32a3
DAM1 <DAM1@aula11-pc18>
2024-10-31 09:16:48 +0100

    He creado la funcion temperatura

--- Reto2.java/src/Reto2/Funciones2.java
+++ Reto2.java/src/Reto2/Funciones2.java
@@ -11,7 +11,32 @@ public class Funciones2 {
 	public static Double fahrenhitAcelsius(Double fahrenheit) {
 		return fahrenheit*1.8+32;
 	}
+	public static double temperatura(Double num,Scanner sc) {
 
+		
+		//Elige opcion
+
+			System.out.println("Selecciona la conversion de temperatura"
+						+ "1. Celsius a Fahrenheint"
+						+ "2. Fahrenheint a Celsius"
+						+ "0. Salir");
+			Funciones.dimeEntero("Dime la opcion", sc);
+			int opcion=sc.nextInt();
+			switch (opcion) {
+			case 0:
+				break;
+
+			case 1:
+				
+				break;
+			case 2:
+				Funciones.dimeDouble("Escribe temperatura", sc);
+				break;
+			}
+		
+		
+		return 0.0;
+	}
 
 	
 	
